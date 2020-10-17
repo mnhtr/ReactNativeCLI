4388c4308dd1f2320ac4641125a02a60446434f1
tringuyenminh123 <mnhtr123>
2020-10-17 09:25:15 +0700

    update

--- component/bmi/BMI.js
+++ component/bmi/BMI.js
@@ -18,36 +18,44 @@ import Dialog from './Dialog';
 const BMI = () => {
   const [gender, setGender] = useState('male');
   const [modalVisible, setModalVisible] = useState(false);
-  const [height,setHeight] = useState(0);
-  const [weight,setWeight] = useState(0);
-  const [age,setAge] = useState(0);
-  const result = (weight/(height/100*height/100)).toFixed(2);;
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [age, setAge] = useState(0);
+  const result = (weight / (((height / 100) * height) / 100)).toFixed(2);
   return (
     <>
       <StatusBar barStyle="default" backgroundColor="#1d2236" />
       <SafeAreaView style={styles.container}>
-      <Dialog modalVisible={modalVisible} setModalVisible={setModalVisible} result={result}
-      setAge={setAge}
-      setHeight={setHeight}
-      setWeight={setWeight}
-      result={result}/>
+        <Dialog
+          modalVisible={modalVisible}
+          setModalVisible={setModalVisible}
+          result={result}
+          setAge={setAge}
+          setHeight={setHeight}
+          setWeight={setWeight}
+          result={result}
+        />
         <View style={{alignItems: 'center'}}>
           <Text style={styles.title}>BMI CALCULATOR</Text>
           <View style={styles.reline} />
         </View>
         <Gender gender={gender} setGender={setGender} />
         <Height height={height} setHeight={setHeight} />
-        <WeightAge  weight={weight} setWeight={setWeight}
-                    age={age} setAge={setAge}/>
-        <TouchableOpacity style={styles.formBottom}
-        onPress={() => {
-          height ===0 && weight ===0 && age ===0 ?
-            setModalVisible(false) : setModalVisible(true);
-        }}
-        >
+        <WeightAge
+          weight={weight}
+          setWeight={setWeight}
+          age={age}
+          setAge={setAge}
+        />
+        <TouchableOpacity
+          style={styles.formBottom}
+          onPress={() => {
+            height === 0 && weight === 0 && age === 0
+              ? setModalVisible(false)
+              : setModalVisible(true);
+          }}>
           <Text style={styles.textBottom}>CALCULATE</Text>
         </TouchableOpacity>
-        
       </SafeAreaView>
     </>
   );
